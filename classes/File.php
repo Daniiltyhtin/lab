1968693dcfb1eed132e69a23f2c485a745efa4ee
Daniil Tyuhtin <daniiltyuhtin@yandex.com>
2024-03-09 17:19:48 +0300

    modified:   classes/File.php 	deleted:    classes/File.php 	deleted:    index.php

--- classes/File.php
+++ classes/File.php
@@ -1,13 +1,29 @@
 <?php 
 
 class File {
-    public function __construct()
+    public $file; //file-name
+    public $fp; //file-pointer
+
+    public function __construct($file, $fp)
     {
-        
+        $this->file = $file;
+        if(!is_writable($this -> file)){
+            echo "Файл {$this -> file} недоступен для записи";
+            exit;
+        }else{
+            $this -> fp = fopen($this -> file, 'a');
+        }
     }
     
     public function __destruct()
     {
+        fclose($this -> fp);
+    }
+
+    public function write(){
         
     }
+
+
+
 }
