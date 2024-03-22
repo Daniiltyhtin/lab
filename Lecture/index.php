ac10f97c687360fe9739ccfb4b842e1de1d32553
Daniiltyhtin <daniiltyuhtin@yandex.com>
2024-03-22 17:04:37 +0300

    проверка на подключение к гит

--- Lecture/index.php
+++ Lecture/index.php
@@ -5,6 +5,7 @@ require 'classes/Car.php';
 $carWhite = new Car("White", 4, 250, "Asus");
 $carBlack = new Car("Black", 4, 230, "Lenovo");
 $carBlue = new Car("Blue", 4, 240, "Nvidia");
+$carGreen = new Car("", 4, 0, "Radeon");
 
 echo Car::$countCar;
 echo Car::getCount();
