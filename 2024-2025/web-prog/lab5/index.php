15c341479ebbd59fb21fb825771f6f55caaf243a
Nemo <daniiltyuhtin@yandex.com>
2024-11-22 13:59:41 +0300

    переделал 5ю и сделал 7

--- 2024-2025/web-prog/lab5/index.php
+++ 2024-2025/web-prog/lab5/index.php
@@ -6,24 +6,35 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <?php
-  $title = "Переменная";
+  require_once('function.php');
+  $title = "Лабоартораня 5";
   echo "<title>$title</title>";
   ?>
 </head>
 
 <body>
-
+  <h2>Функции для работы со строками</h2>
   <?php
+  $stringTest = "Привет Мир!";
 
-  echo "Проверка";
+  echo "<p>Тестовая строка = $stringTest</p>";
 
-  // комментарий
+  echo "<h3>Функция <b>strlen</b></h3>" .
+    "<p>Длина строки 'Привет' = " . strlen("Привет") . "</p>";
 
-  /* комментарий побольше:
-  * 1)
-  * 2)
-  * 3)
-  **/
+  echo "<h3>Функция explode</h3>" .
+    "<p>Разбивает строку на массив элементов с выбранным разделителем</p>";
+  var_dump(explode(" ", $stringTest));
+
+  echo "<h3>Функция implode</h3>" .
+    "<p>Обратная explode функция</p>";
+  echo (implode(explode(" ", $stringTest)));
+
+  echo "<h3>Функция htmlspecialchars</h3>" .
+    "<p>Преобразует специальные символы в HTML-сущности</p>";
+  $sringHtml = '<p class="exaple">Hello word<p>';
+  echo htmlspecialchars($sringHtml);
+  echo htmlentities($sringHtml);
 
   ?>
 
